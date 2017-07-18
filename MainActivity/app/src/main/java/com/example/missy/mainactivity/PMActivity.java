45956797aa8a218ed343834fa10045acf69ebe8f
HyeonJiShin <tlsguswl10@naver.com>
2017-07-18 01:28:42 -0700

    UI
    Login, PM 완성

--- MainActivity/app/src/main/java/com/example/missy/mainactivity/PMActivity.java
+++ MainActivity/app/src/main/java/com/example/missy/mainactivity/PMActivity.java
@@ -13,13 +13,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class PMActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_pm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.pm, menu);
         return true;
     }
 
